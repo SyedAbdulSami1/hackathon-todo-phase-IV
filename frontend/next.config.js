326194ea2b6a96a58e4787186ab0491dbca9a2d4
SyedAbdulSami1 <samiwpp@gmail.com>
2026-03-17 06:58:03 +0500

    today stop

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: '../.next',
     images: {
         domains: ['api.dicebear.com', 'yt3.googleusercontent.com'],
     },
--- next.config.js
+++
@@ -1,7 +0,0 @@
-const nextConfig = require('./frontend/next.config.js');
-
-module.exports = {
-  ...nextConfig,
-  // Ensure Next.js looks for pages/app in the frontend directory
-  dir: './frontend',
-};
